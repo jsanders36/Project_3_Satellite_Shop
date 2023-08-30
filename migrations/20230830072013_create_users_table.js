4d1409ff9d7246a5a6ce3db23050efcf8748c7af
Connor Smith <cmsmith1217@gmail.com>
2023-08-30 02:39:46 -0500

    made database migration files; still need to edit package migration file.

---
+++ migrations/20230830072013_create_users_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('users', table => {
+        table.increments();
+        table.string('username');
+        table.string('password');
+        table.boolean('is_admin');
+        table.string('profile_image');
+    })  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users');
+};
