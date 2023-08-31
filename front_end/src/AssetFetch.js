a26b75712b7e0be1f433b975b756b88702ae2625
codewithchewy <srepp@twu.edu>
2023-08-31 11:10:38 -0700

    added AssetFetch and imported into AssetList component, added express cors

---
+++ front_end/src/AssetFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+
+const AssetFetch = (name) => {
+    const [assetData, setAssetData] = useState([]);
+    const [error, setError] = useState(null);
+
+    const url = "http://localhost:8085/assets";
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error("Asset name not found");
+                }
+                const data = await response.json();
+                setAssetData(data);
+
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setError(error.message);
+            }
+        }
+        fetchData();
+    }, [name]);
+
+    return { assetData, error };
+};
+
+export default AssetFetch;
