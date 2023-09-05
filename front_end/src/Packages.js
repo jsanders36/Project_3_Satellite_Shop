a0fe7185de99f5735fbca84beb67b6784a091d15
eric-baldwin <erictbaldwinjr@gmail.com>
2023-09-05 12:57:33 -0600

    finished the packages page.

--- front_end/src/Packages.js
+++ front_end/src/Packages.js
@@ -1,24 +1,14 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import Navbar from './Navbar';
-import Dropdown from './Dropdown';
 import { useAssetContext } from './addToPackage';
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
   return (
@@ -36,20 +26,18 @@ function Home() {
 export default function Packages() {
   const { addAsset, setAddAsset } = useAssetContext();
   const navigate = useNavigate();
-  //console.log("addAsset:", addAsset);
 
-  const [selectedAssets, setSelectedAssets] = useState();
   const [packageDescriptionInput, setPackageDescriptionInput] = useState('');
   const [packageNameInput, setPackageNameInput] = useState('');
 
-  const packageDatabase = "http://localhost:8085/packages";
-  const joinTableDatabase = "http://localhost:8085/assets_packages";
+  const packageDatabase = 'http://localhost:8085/packages';
+  const joinTableDatabase = 'http://localhost:8085/assets_packages';
 
   const handleDescriptionChange = (e) => {
     setPackageDescriptionInput(e.target.value);
   }
 
-  const handleNameChange= (e) => {
+  const handleNameChange = (e) => {
     setPackageNameInput(e.target.value);
   }
 
@@ -57,38 +45,36 @@ export default function Packages() {
     let numPackagesInDb;
 
     await fetch(packageDatabase, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        "name": packageNameInput,
-        "mission_description": packageDescriptionInput,
-        "approval_status": false,
-        "users_id": 1
-      })
-    })
-    .then((response) => response.json())
+        name: packageNameInput,
+        mission_description: packageDescriptionInput,
+        approval_status: false,
+        users_id: 1,
+      }),
+    }).then((response) => response.json());
 
     await fetch(packageDatabase)
-    .then((response) => response.json())
-    .then((packageDbLength) => numPackagesInDb = packageDbLength.length)
+      .then((response) => response.json())
+      .then((packageDbLength) => (numPackagesInDb = packageDbLength.length));
 
     let numSatsToSend = addAsset.length;
-    for(let i = 0; i < numSatsToSend; i++) {
+    for (let i = 0; i < numSatsToSend; i++) {
       await fetch(joinTableDatabase, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          "assets_id": addAsset[i].id,
-          "packages_id": numPackagesInDb
-        })
-      })
-      .then((response) => response.json())
+          assets_id: addAsset[i].id,
+          packages_id: numPackagesInDb,
+        }),
+      }).then((response) => response.json());
     }
-  }
+  };
 
   return (
     <>
@@ -100,7 +86,12 @@ export default function Packages() {
       <main>
         <Box
           sx={{
-            bgcolor: 'red',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            bgcolor: 'rgba(0, 0, 0, 0.9)',
             backgroundImage: 'url(https://i.pinimg.com/originals/9d/73/af/9d73af36e486def0f4c81aa372b8655c.gif)',
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
@@ -109,107 +100,117 @@ export default function Packages() {
             pb: 6,
           }}
         >
-          <Container sx={{
-            bgcolor: 'rgba(0, 0, 0, 0.5)',
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            padding: '100px',
+            bgcolor: 'transparent',
+            marginTop: '-500px',
+
           }}>
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="white"
-              gutterBottom
-            >PACKAGES
-            </Typography>
-            {/* <Box sx={{ padding: '20px' }}>
-              <Typography variant="h4">Packages</Typography>
-              {addAsset.map((asset) => (
-                <div key={asset.name}>
-                  <Typography>{asset.name}</Typography>
-                  {console.log(asset.name)}
-                </div>
-              ))}
-            </Box> */}
-            <Typography variant="h5" align="center" color="white" paragraph>
-            </Typography>
-            <Typography variant="h5" align="center" color="white" paragraph>
-              Achieving and Sustaining Space Superiority: Your Mission is Our Commitment.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            ></Stack>
-          </Container>
-        </Box>
-        <Box sx={{
-          bgcolor: 'rgba(0, 0, 0, 0.9)',
-          backgroundImage: 'url(https://cdn.wallpapersafari.com/88/66/Uowyg0.jpg)',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          py: 8
-        }}>
-          <Box sx={{ padding: '20px', maxHeight: '200px', overflowY: 'scroll', bgcolor: 'white' }}>
-            <Typography variant="h6">Packages:</Typography>
-            <form>
-              <ul>
-              {addAsset.map((asset) => (
-                <li key={asset?.id}>{asset?.name}
-                   <Button
+            <Container
+              sx={{
+                bgcolor: 'rgba(0, 0, 0, 0.5)',
+                mb: 10,
+              }}
+            >
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="white"
+                gutterBottom
+              >
+                PACKAGES
+              </Typography>
+              <Typography variant="h5" align="center" color="white" paragraph></Typography>
+              <Typography variant="h5" align="center" color="white" paragraph>
+                Achieving and Sustaining Space Superiority: Your Mission is Our Commitment.
+              </Typography>
+            </Container>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'top',
+                alignItems: 'center',
+                border: '1px solid #2196f3',
+                boxShadow: '0 0 20px rgba(33, 150, 243, 0.9)',
+                padding: '40px',
+                maxHeight: '200px',
+                overflowY: 'scroll',
+                bgcolor: 'white',
+                width: '75%',
+              }}
+            >
+              <Typography variant="h4">Package Loadout:</Typography>
+              <form>
+                <ul>
+                  {addAsset.map((asset) => (
+                    <li key={asset?.id}>
+                      {asset?.name}{' ----- '}
+                      <Button
+                        padding="10px"
+                        variant="contained"
+                        color="primary"
                         onClick={(e) => {
                           e.preventDefault();
-                          const releasedAsset = addAsset.filter(
-                            (element) => element?.id !== asset?.id
-                          );
+                          const releasedAsset = addAsset.filter((element) => element?.id !== asset?.id);
                           setAddAsset(releasedAsset);
-                          navigate("/packages");
+                          navigate('/packages');
                         }}
-                  >
-                    Remove Asset From Package
+                      >
+                        Remove Asset From Package
                       </Button>
-                </li>
-              ))}
-              </ul>
-              <label for="mission_description"> Mission Description
-                <input type="text" id="mission_description" placeholder="Please add a mission description for your package..." onChange={handleDescriptionChange} value={packageDescriptionInput} />
-                <input type="text" id="package_name" placeholder="Name your package..." onChange={handleNameChange} value={packageNameInput} />
-                <button onClick={(e) => {
-                  e.preventDefault();
-                  packageSubmission();
-                }}>Submit Package Request</button>
-              </label>
-            </form>
+                    </li>
+                  ))}
+                </ul>
+                <label htmlFor="mission_description"> Mission Description:
+                  <input
+                    type="text"
+                    id="mission_descriptions"
+                    placeholder="Add a mission description"
+                    onChange={handleDescriptionChange}
+                    value={packageDescriptionInput}
+                  />
+                  <br></br>
+                  Name:
+                  <input
+                    type="text"
+                    id="package_name"
+                    placeholder="Name your package..."
+                    onChange={handleNameChange}
+                    value={packageNameInput}
+                  />
+                  <br></br>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      packageSubmission();
+                    }}
+                  >
+                    Submit Package Request
+                  </button>
+                </label>
+              </form>
+            </Box>
           </Box>
-          <Container
-            sx={{
-              py: 8
-            }}
-            maxWidth="lg">
-
-          </Container>
         </Box>
       </main>
-      {/* Footer */}
-      <Box sx={{
-        bgcolor: 'primary.main',
-        pt: 8,
-        pb: 6,
-      }}
-        component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="white"
-          component="p"
-        >
+      <Box
+        sx={{
+          bgcolor: 'primary.main',
+          pt: 8,
+          pb: 6,
+        }}
+        component="footer"
+      >
+        <Typography variant="subtitle1" align="center" color="white" component="p">
+          {/* ... */}
         </Typography>
         <Home />
       </Box>
-      {/* End footer */}
     </>
   );
-
 }
